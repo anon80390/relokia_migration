ce67a3b8bbc292f1afcbf0ebfeb322b08bfbed68
Vadym <v.tylnyi@relokia.com>
2025-03-25 15:22:26 +0200

    first commit

--- code/index2.php
+++ code/index2.php
@@ -1,8 +1,22 @@
 <?php
-$arr = [10, 10, 20, 21, 11, 10 , 11];
-$arr2 = [10, 15, 20, 21, 11, 12 , 11];
-function toMonolit($arr1, $arr2){
+$array = ['mount', 'ore', 'green', 'road', 'telephone', 'mom', 'more'];
+$letter = 'm';
 
+function wordArray($array, $letter){
+    $res = [];
+
+    foreach($array as $item){
+        if(str_starts_with($item, $letter)){
+           array_push($res, $item);
+        }
+
+    }
+return $res;
 }
+$output = wordArray($array, $letter);
+
+
+
+//$filtered_users = array_filter($users, "updateUsers");
 
 $let = '';
